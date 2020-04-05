8fd3f48ebf05ccc50f4fc52d4785de4403d000fe
Megan Horsfield <meganthadev@gmail.com>
2020-04-05 00:36:02 +0000

    Done.

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,9 @@
 class CreateActors < ActiveRecord::Migration[5.1]
-  def change 
-    create_table :artists do |t|
+  
+  def change
+    create_table :actors do |t|
       t.string :first_name
-      t.string :last_name 
-    end  
-  end   
-end   
+      t.string :last_name
+    end
+  end
+end
