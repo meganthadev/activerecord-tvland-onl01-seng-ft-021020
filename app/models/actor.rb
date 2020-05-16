3f3bcbb97238d336c231c55e1267da71a542e0a3
Megan Horsfield <meganthadev@gmail.com>
2020-05-16 05:36:09 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,8 +7,8 @@ class Actor < ActiveRecord::Base
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-    @@all << self 
-  end   
+    @@all << self
+  end
 
   def full_name
     "#{self.first_name} #{self.last_name}"
