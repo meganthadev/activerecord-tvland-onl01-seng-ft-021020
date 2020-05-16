2685ce8fbc0bafaff799ecd5c7a2838f9af149a9
Megan Horsfield <meganthadev@gmail.com>
2020-05-16 05:35:14 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,7 @@ class Actor < ActiveRecord::Base
 
   attr_accessor :first_name, :last_name
   has_many :characters
-  has_many :shows, through: :characters  
+  has_many :shows, through: :characters
 
   def full_name
     "#{self.first_name} #{self.last_name}"
